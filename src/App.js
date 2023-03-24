4732258723519207c8de7e2924d1ac4c966b4410
Anikkhan-web <anikahmed.ef@gmail.com>
2023-03-24 14:36:20 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h2 className='text-6xl'>anik</h2>
+
+    </div >
+  );
+}
+
+export default App;
